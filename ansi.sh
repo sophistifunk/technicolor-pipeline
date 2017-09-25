ae58d41087d3b22240ab994bdc5d85daabbf2f23
Josh McDonald <josh@expantra.com>
2017-09-25 13:55:20 +1000

    Update ansi.sh

--- ansi.sh
+++ ansi.sh
@@ -20,6 +20,7 @@ echo -e "Example \033[47;30m      WHITE       \033[0m BG"
 echo -e ""
 echo -e "Time to mix colours and urls :)"
 
+echo -e "\032[34m Green text \033[0m with nothing interesting alongside."
 echo -e "\032[34m Green text \033[0m with a non-url www.example.org alongside."
 echo -e "\032[34m Green text \033[0m with a url http://www.example.org/ alongside."
 echo -e "\032[34m Green text with a url http://www.example.org/ inside. \033[0m"
